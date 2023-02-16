7fc4d1a235cafc514c3af50c46a577eb61e7baa3
David Praise <david.praise2323@gmail.com>
2023-02-16 22:35:32 +0200

    added link

--- shopping-back/shopping/settings.py
+++ shopping-back/shopping/settings.py
@@ -140,6 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://django-shopping-backend.herokuapp.com',
 ]
 
 # Configure Django App for Heroku.
