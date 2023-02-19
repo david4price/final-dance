5736be1375aa33534b96c5f689d9a271aee60894
David Praise <david.praise2323@gmail.com>
2023-02-19 21:56:35 +0200

    fixed archive function

--- shopping-back/product/views.py
+++ shopping-back/product/views.py
@@ -50,23 +50,6 @@ def single_product(request, pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['PUT'])
-def archive_Product(request, pk):
-    try:
-        product = Product.objects.get(id=pk)
-    except Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    # archive a single product
-    if request.method == 'PUT':
-        serializer = ProductSerializer(instance=product, data=request.data)
-        if serializer.is_valid():
-            serializer.archive = True
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 @api_view(['GET', 'POST'])
 def cartItems(request):
     # get cart items
