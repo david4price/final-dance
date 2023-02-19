3339300d36f2c96e46c68e77f5307ca8b643b86d
David Praise <david.praise2323@gmail.com>
2023-02-19 22:01:21 +0200

    fixed archive function

--- shopping-back/product/urls.py
+++ shopping-back/product/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.products),
-    path('products-archive/<int:pk>', views.single_product),
+    path('products-archive/<int:pk>/', views.single_product),
 
     path('cart-items/', views.cartItems),
     path('cart-items/<int:pk>/', views.single_CartItem),
